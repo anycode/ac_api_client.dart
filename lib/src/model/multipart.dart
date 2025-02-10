eae58807c38642741d7dbc3ffb576def8e229bca
Martin Edlman <ac@anycode.dev>
2025-02-10 23:28:44 +0100

    Constructor for creating media multipart field. Utility function to convert map to list of multipart fields
    Signed-off-by: Martin Edlman <ac@anycode.dev>

--- lib/src/model/multipart.dart
+++ lib/src/model/multipart.dart
@@ -1,6 +1,9 @@
+/// Type of a field in a multipart body
 enum MultipartFieldType { field, media }
 
+/// A field in a multipart body
 class MultipartField {
+  /// Creates a new [MultipartField]
   MultipartField({
     required this.name,
     required this.value,
@@ -9,9 +12,22 @@ class MultipartField {
     this.filename,
   });
 
+  /// Creates a new [MultipartField] of type [MultipartFieldType.media]
+  MultipartField.media({
+    String? name,
+    required this.value,
+    this.contentType,
+    required this.filename,
+  })  : type = MultipartFieldType.media,
+        name = name ?? filename!;
+
   final String? filename;
   final String name;
   final dynamic value;
   final MultipartFieldType type;
   final String? contentType;
 }
+
+/// Maps a [Map] to a list of [MultipartField]
+List<MultipartField> mapToMultipartFields(Map<String, dynamic> map) =>
+    map.entries.map((e) => MultipartField(name: e.key, value: e.value)).toList();
